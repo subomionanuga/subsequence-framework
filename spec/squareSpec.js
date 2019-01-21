3bddd0f4e6d591aa6d89976ea7fc97973b59da98
Subomi Onanuga <subomi_onanuga@yahoo.co.uk>
2019-01-21 16:01:39 +0000

    Test passing for area

--- spec/squareSpec.js
+++ spec/squareSpec.js
@@ -1,8 +1,17 @@
 describe('Square', function() {
   var square
 
+  // beforeEach(function() {
+  //   square = new Square(5)
+  // })
+
   testWill('check the length of one square side', function() {
     square = new Square(5)
     expect(square.size).toBeEqualTo(5)
+  })
+
+  testWill('check the area of the square', function() {
+    square = new Square(5)
+    expect(square.area()).toBeEqualTo(25)
   })
 })
