b9c709fefb6c20cb8036902331e79eae35d44da3
Subomi Onanuga <subomi_onanuga@yahoo.co.uk>
2019-01-21 15:58:20 +0000

    Written framework piece for matcher

--- spec/squareSpec.js
+++ spec/squareSpec.js
@@ -1,5 +1,8 @@
 describe('Square', function() {
+  var square
+
   testWill('check the length of one square side', function() {
-  //   expect(square.size).toBeEqualTo(5)
+    square = new Square(5)
+    expect(square.size).toBeEqualTo(5)
   })
 })
